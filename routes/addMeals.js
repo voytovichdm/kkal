77d49ccee98173689091fe0fb07b84821f40dd19
Dmytro <voitovych.dm.m@gmail.com>
2022-05-18 14:34:15 +0300

    statistics

--- routes/addMeals.js
+++ routes/addMeals.js
@@ -1,37 +1,49 @@
 const express = require('express')
 const model = require('../models')
-// const {where} = require("sequelize/types");
 const router = express.Router()
 
 router.post('/add/meal',
     async (req, res) => {
         try {
-            await model.Meals.create({
-                name: req.body.name,
-                protein_a: req.body.protein_a,
-                protein_v: req.body.protein_v,
-                fats_a: req.body.fats_a,
-                fats_v: req.body.fats_v,
-                carbohydrates_f: req.body.carbohydrates_f,
-                carbohydrates_s: req.body.carbohydrates_s,
-                calories: req.body.calories,
-                meal: req.body.meal
-            })
-
-            let user = await model.Users.findOne({
-                where: {
-                    name: "Dmytro"
-                }
-            });
-            let food = await model.Meals.findOne({
+            let information = await model.Foods.findOne({
                 where: {
                     name: req.body.name
                 }
             })
-            let result = await user.addMeals(food);
 
+            if(information) {
+                information.protein_a = information.protein_a / 100 * req.body.grams,
+                information.protein_v = information.protein_v / 100 * req.body.grams,
+                information.fats_a = information.fats_a / 100 * req.body.grams,
+                information.fats_v = information.fats_v / 100 * req.body.grams,
+                information.carbohydrates_f = information.carbohydrates_f / 100 * req.body.grams,
+                information.carbohydrates_s = information.carbohydrates_s / 100 * req.body.grams,
+                information.calories = information.calories / 100 * req.body.grams
+            } else {
+                res.status(404).json({
+                    message: "Not found"
+                })
+            }
 
-            res.status(200).json(food)
+            let meal = await model.Meals.create({
+                name: information.name,
+                protein_a: information.protein_a,
+                protein_v: information.protein_v,
+                fats_a: information.fats_a,
+                fats_v: information.fats_v,
+                carbohydrates_f: information.carbohydrates_f,
+                carbohydrates_s: information.carbohydrates_s,
+                calories: information.calories,
+                meal: req.body.meal
+            })
+
+            let user = await model.Users.findByPk(req.body.id);
+            let food = await model.Meals.findByPk(meal.id)
+            await user.addMeals(food);
+
+            res.status(200).json({
+                message: 'Saved'
+            })
         } catch (err) {
             console.log(err.message)
         }
--- routes/statistics.js
+++ routes/statistics.js
@@ -1,18 +1,79 @@
 const express = require('express')
 const model = require('../models')
+const { Op } = require('sequelize');
 const router = express.Router()
 
-router.get('/day',
+router.post('/statistics',
     async (req, res) => {
     try {
-        let result = await model.Meals.findAll({
-            where: {
-                meal: '1',
+        const startedDate = new Date(req.body.dateStart);
+        const endDate = new Date(req.body.dateFinish);
 
-            }
-        })
+        let resultDBTest = []
 
-        res.status(200).json(result)
+        if(req.body.meal) {
+            let resultDB = await model.Users.findAll({
+                where: {
+                    name: req.body.name
+                },
+                attributes: ['id', 'name', 'email'],
+                include: {
+                    model: model.Meals,
+                    where : [{ meal: req.body.meal },
+                        {"createdAt" : {[Op.between] : [startedDate , endDate]}}],
+                    // attributes: [
+                    //         [sequelize.fn("SUM", sequelize.col("protein_a")), 'res_protein_a'],
+                    //         [sequelize.fn("SUM", sequelize.col("protein_v")), 'res_protein_a']
+                    //     ]
+                }
+            })
+            resultDBTest = resultDB
+        } else {
+            let resultDB = await model.Users.findAll({
+                where: {
+                    name: req.body.name
+                },
+                attributes: ['id', 'name', 'email'],
+                include: {
+                    attributes: {
+                      exclude: ["id", 'meal', 'name', 'createdAt']
+                    },
+                    model: model.Meals,
+                    raw: true,
+                    where : [
+                        {"createdAt" : {[Op.between] : [startedDate , endDate]}}]
+                }
+            })
+            resultDBTest = resultDB
+        }
+
+        let objMeals = resultDBTest[0]
+
+        if(objMeals) {
+            let result = {
+                protein_a: 0,
+                protein_v: 0,
+                fats_a: 0,
+                fats_v: 0,
+                carbohydrates_f: 0,
+                carbohydrates_s: 0,
+                calories: 0,
+            };
+
+            objMeals.Meals.forEach(meal=>{
+                result.protein_a += meal['protein_a'];
+                result.protein_v += meal['protein_v'];
+                result.fats_a += meal['fats_a'];
+                result.fats_v += meal['fats_v'];
+                result.carbohydrates_f += meal['carbohydrates_f'];
+                result.carbohydrates_s += meal['carbohydrates_s'];
+                result.calories += meal['calories'];
+            })
+
+            res.status(200).json(result)
+        } else {
+            res.status(404).json({ message: 'Not found'})
+        }
     } catch (err) {
         console.log(err.message)
     }
